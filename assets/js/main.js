5bddd21cd20ca269f5c36f6a4a0ea19f7d7a6d16
Ana Laura de Araujo <anaraujo0847@gmail.com>
2019-10-18 09:29:57 -0300

    Including new to-dos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
 // Check of specific to-dos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
-//Click on X to delete todo
-$("span").click(function(event) {
+// Click on X to delete to-do
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		// Grabbing new to-do text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// Create a new li and add to ul
+		$("ul").append("<li><span>X</span> " + todoText + "</li>");	
+	}
 });
